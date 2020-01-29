b5fee118faeab7e79855a47cc23114be44e0a00f
Fredrik Westmark <fredrik.westmark@gmail.com>
2020-01-29 11:02:28 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,79 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {promises as fs} from 'fs';
+import * as core from '@actions/core';
+import {exec} from '@actions/exec';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+async function isDir(path: string): Promise<boolean> {
+  return fs.stat(path).then(
+    (s) => s.isDirectory(),
+    () => false
+  );
 }
 
-run()
+async function isFile(path: string): Promise<boolean> {
+  return fs.stat(path).then(
+    (s) => s.isFile(),
+    () => false
+  );
+}
+
+const fixUrl = (url: string): string =>
+  `//${url.replace(/^\/*/, '').replace(/\/*$/, '')}/`;
+
+async function run(): Promise<void> {
+  const dir = core.getInput('dir');
+  if (dir) {
+    process.chdir(dir);
+  }
+
+  const npmUrl = core.getInput('npmurl');
+  if (!npmUrl) {
+    core.setFailed("'npmurl' must be set");
+    return;
+  }
+
+  const npmToken = core.getInput('token');
+
+  if (!npmToken) {
+    core.setFailed("'token' input not set");
+    return;
+  }
+
+  const isPublic = Boolean(core.getInput('public'));
+
+  const packageFile = './package.json';
+  const pkg = JSON.parse((await fs.readFile(packageFile)).toString());
+  if (!pkg.name.match(/^@tv4-media\//)) {
+    core.setFailed('Cannot publish packages outside @tv4-media scope');
+    return;
+  }
+
+  if (Boolean(pkg.private) === isPublic) {
+    core.setFailed(
+      "'public' input parameter and package.json 'private' parameter mismatch"
+    );
+    return;
+  }
+
+  await exec(`npm config set @tv4-media:registry https:${fixUrl(npmUrl)}`);
+  await exec(`npm config set '${fixUrl(npmUrl)}:_authToken' '${npmToken}'`);
+
+  await exec('npm whoami');
+
+  /* check if the deps where installed in a previous step */
+  const isInstalled = await isDir('node_modules');
+
+  if (!isInstalled) {
+    if (await isFile('package-lock.json')) {
+      await exec('npm ci');
+    } else {
+      await exec('npm install');
+    }
+  }
+
+  await exec('npm publish');
+}
+
+run().catch((error) => {
+  console.error('Action failed', error);
+  core.setFailed(error.message);
+});
